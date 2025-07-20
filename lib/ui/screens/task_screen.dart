8a6948ff0d96ee65e7f676874b8256ea3bb07794
althafahmd <althafahamed47@gmail.com>
2025-07-20 21:29:39 +0530

    Update task_screen.dart

--- lib/ui/screens/task_screen.dart
+++ lib/ui/screens/task_screen.dart
@@ -31,79 +31,81 @@ class _TaskScreenState extends State<TaskScreen> {
     final box = Hive.box<TaskModel>('tasks');
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                top: 60.0,
-                right: 30.0,
-                bottom: 30.0,
-                left: 30.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    maxRadius: ScreenSizeConfig.screenWidth * 0.085,
-                    backgroundColor: AppColors.pureWhite,
-                    child: Icon(
-                      Icons.list,
-                      color: AppColors.cardColor,
-                      size: ScreenSizeConfig.screenWidth * 0.1,
-                    ),
+        child: ValueListenableBuilder(
+          valueListenable: box.listenable(),
+          builder: (context, Box<TaskModel> updatedBox, _) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                    top: 60.0,
+                    right: 30.0,
+                    bottom: 30.0,
+                    left: 30.0,
                   ),
-                  Text('Todoey', style: AppTypography.titleLarge),
-                  Text(
-                    '${box.length} Tasks',
-                    style: AppTypography.labelTextStyle.copyWith(
-                      color: AppColors.primaryBlue,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-                decoration: BoxDecoration(
-                  color: AppColors.primaryBG,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50.0),
-                    topRight: Radius.circular(50.0),
-                  ),
-                ),
-                child: ValueListenableBuilder(
-                  valueListenable: box.listenable(),
-                  builder: (context, Box<TaskModel> updatedBox, _) {
-                    if (updatedBox.isEmpty) {
-                      return Center(
-                        child: Text(
-                          'Add Tasks',
-                          style: AppTypography.labelTextStyle.copyWith(
-                            color: AppColors.textColor,
-                          ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        maxRadius: ScreenSizeConfig.screenWidth * 0.085,
+                        backgroundColor: AppColors.pureWhite,
+                        child: Icon(
+                          Icons.list,
+                          color: AppColors.cardColor,
+                          size: ScreenSizeConfig.screenWidth * 0.1,
                         ),
-                      );
-                    }
-                    return ListView.builder(
-                      itemCount: updatedBox.length,
-                      physics: BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      itemBuilder: (context, index) {
-                        final task = updatedBox.getAt(index)!;
-                        return TaskTile(
-                          taskName: task.name,
-                          onDelete: () => task.delete(),
-                        );
-                      },
-                    );
-                  },
+                      ),
+                      Text('Todoey', style: AppTypography.titleLarge),
+                      Text(
+                        '${updatedBox.length} Tasks',
+                        style: AppTypography.labelTextStyle.copyWith(
+                          color: AppColors.primaryBlue,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-          ],
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                      vertical: 20.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryBG,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50.0),
+                        topRight: Radius.circular(50.0),
+                      ),
+                    ),
+                    child: updatedBox.isEmpty
+                        ? Center(
+                            child: Text(
+                              'Add Tasks',
+                              style: AppTypography.labelTextStyle.copyWith(
+                                color: AppColors.textColor,
+                              ),
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: updatedBox.length,
+                            physics: BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemBuilder: (context, index) {
+                              final task = updatedBox.getAt(index)!;
+                              return TaskTile(
+                                taskName: task.name,
+                                onDelete: () => task.delete(),
+                              );
+                            },
+                          ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
